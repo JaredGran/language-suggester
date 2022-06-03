756e823b3c52d07ae5889fa98dca91911dd1a196
JaredGran <jared.carper@gmail.com>
2022-06-03 09:07:51 -0700

    stuck working on js

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  $("form#language").submit(function(event) {
+    event.preventDefault();
+    const  = $("input#yes").val();
+    const  = $("imput#no").val();
+  });
+});
