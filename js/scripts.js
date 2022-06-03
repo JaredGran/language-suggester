57ce84dcef0eadaa17f6a1e659d69b08c4c9621a
JaredGran <jared.carper@gmail.com>
2022-06-03 12:50:00 -0700

    back from lunch

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,25 @@
+
+let yes = parseInt(5)
+let no = parseInt(0)
+
+
+
+
+
+
+
+
+
+
+
+
+
 $(document).ready(function() {
   $("form#language").submit(function(event) {
     event.preventDefault();
     const  = $("input#yes").val();
-    const  = $("imput#no").val();
-    let yes = parseInt(5)
-    let no = parseInt(0)
+   
+
   
   });
 });
